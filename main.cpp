3bd7b3af5f0f3600ca8ad2235362610c3b9503a5
AndrzejSwietek2001 <mr.andi321@gmail.com>
2021-06-14 22:42:44 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+//  TODO: class Board, class Piece, class Pawn, class Figure: Kingg ,Queen, Bish, Rook, Knight
