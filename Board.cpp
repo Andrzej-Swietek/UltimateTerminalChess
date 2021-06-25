6ae3a34a365a877677e2aa6e8c545497e003cb50
Jakub <kcieslikiewicz@interia.pl>
2021-06-25 15:40:16 +0200

    adresacja szachownicy, wieza, hetman, skoczek

--- Board.cpp
+++ Board.cpp
@@ -76,131 +76,219 @@ void Board::turn(string move) {
     else if ( move.length() != 5 ) { cout << "Wrong Format" <<endl;return; }
     else {
         string what = move.substr(0, move.find("-"));
-        string where = move.substr(move.find("-")+1, move.length());
+        string where = move.substr(move.find("-") + 1, move.length());
         cout << what << "    |   " << where << endl;
 
         string sWhatY = &what[1];
-        int whatY = stoi(sWhatY)-1;
-        int whatX = int(what[0]) - int('a');
+        int whatY = stoi(sWhatY) - 1;
+        int whatX = 7-(int(what[0]) - int('a'));
 
-//        cout << whatX <<"  "<< whatY << endl;
-        cout<< "[ Chosen Piece ]: " <<fields[ whatY ][ whatX ].name << endl;
+        cout << "[ Chosen Piece ]: " << fields[whatY][whatX].name << endl;
 
+        string sWhereY = &where[1];
+        int whereY = stoi(sWhereY) - 1;
+        int whereX = 7-(int(where[0]) - int('a'));
 
-        if ( fields[ whatY ][ whatX ].name == "R" ){
-
-        } else if (fields[ whatY ][ whatX ].name == "N") {
-
-        } else if (fields[ whatY ][ whatX ].name == "B") {
-            string sWhereY = &where[1];
-            int whereY = stoi(sWhereY)-1;
-            int whereX = int(where[0]) - int('a');
-            if(whereX>7 || whereX<0 || whereY>7 || whereY<0){
-                cout<<RED<<"Wrong format."<<RESET<<endl;
-            }
-            //int px=whatX;
-            //int py=whatY;
-            //bool czyWszystkoOk = false;
-
-
-
-            cout<<whatY<<endl;
-            cout<<whatX<<endl;
-            cout<<whereY<<endl;
-            cout<<whereX<<endl;
-
-/* 1 cw
-     cout << "CW1 "<<endl;
-            while ( px < 7 && py > 0 ) {
-                if(py==whereY && px==whereX){
-
-                    czyWszystkoOk= true;
-                    break;
-                }
-                else{
-                    py--;
-                    px++;
-                }
-            }
-             px=whatX;
-             py=whatY;
-// 2 cw
-            cout << "CW2 "<<endl;
-            while ( px > 0 && py > 0 ) {
-                if(px==whereX && py==whereY){
-                    czyWszystkoOk= true;
-                    break;
-                }
-                else{
-                    px--;
-                    py--;
-                }
-            }
-             px=whatX;
-             py=whatY;
-// 3 cw
-            cout << "CW3 "<<endl;
-            while ( px > 0 && py < 7 ) {
-                if(px==whereX && py==whereY){
-                    czyWszystkoOk= true;
-                    break;
-                }
-                else{
-                    px--;
-                    py++;
-                }
-            }
-             px=whatX;
-             py=whatY;
-// 4 cw
-            cout << "CW4 "<<endl;
-            while ( px < 7 && py < 7 ) {
-                if(px==whereX && py==whereY){
-                    czyWszystkoOk= true;
-                    break;
-                }
-                else{
-                    px++;
-                    py++;
-                }
-            }
-
-            if(czyWszystkoOk==0 ){
-                cout<<"Wrong format..."<<endl;
-                return;
-            }
-*/
-
-            int tmp_x = whatX;
-            int tmp_y = whatY;
-            if(abs(whereX-whatX)!=abs(whereY-whatY)){
-                cout<<RED<<"Wrong format"<<RESET<<endl;
-                return;
-            }
-            else{
-                int dX = (whatX > whereX)? -1 : 1;
-                int dY = (whatY > whereY)? -1 : 1;
-                tmp_x += dX;   tmp_y += dY;
-                while( tmp_x != whereX && tmp_y != whereY) {
-                    if( fields[tmp_y][tmp_x].name != "E" ) {
-                        cout << RED << "ERROR" << RESET << endl; return; }
-                    tmp_x += dX;   tmp_y += dY;
-
+        if (whereX > 7 || whereX < 0 || whereY > 7 || whereY < 0) {
+            cout << RED << "Wrong format." << RESET << endl;
+            return;
+        } else if (fields[whatY][whatX].name == "R") {
+            if(whatX==whereX && whatY!=whereY)
+            {
+                int tmp_x = whatX;
+                int tmp_y = whatY;
+                int dY = (whatY > whereY) ? -1 : 1;
+                tmp_y += dY;
+                while(tmp_y!=whereY)
+                {
+                    if (fields[tmp_y][tmp_x].name != "E") {
+                        cout << RED << "ERROR" << RESET << endl;
+                        return;}
+                    tmp_y += dY;
                 }
                 fields[whereY][whereX] = fields[whatY][whatX];
                 fields[whatY][whatX].name = "E";
 
             }
+            else if(whatX!=whereX && whatY==whereY)
+            {
+                int tmp_x = whatX;
+                int tmp_y = whatY;
+                int dX = (whatX > whereX) ? -1 : 1;
+                tmp_x += dX;
+                while(tmp_x!=whereX)
+                {
+                    if (fields[tmp_y][tmp_x].name != "E") {
+                        cout << RED << "ERROR" << RESET << endl;
+                        return;}
+                    tmp_x += dX;
+                }
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else
+            {
+                cout << RED << "Wrong format" << RESET << endl;
+                return;}
 
-// Delta X i Y
+
+        } else if (fields[whatY][whatX].name == "N") {
+
+            if(whereX-whatX==1 && whereY-whatY==2)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(whereX-whatX==1 && whereY-whatY==-2)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                    return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(whereX-whatX==-1 && whereY-whatY==2)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                    return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(whereX-whatX==-1 && whereY-whatY==-2)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                    return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(whereX-whatX==2 && whereY-whatY==1)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                    return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(whereX-whatX==2 && whereY-whatY==-1)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                    return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(whereX-whatX==-2 && whereY-whatY==1)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                    return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(whereX-whatX==-2 && whereY-whatY==-1)
+            {
+                if(fields[whereY][whereX].name!="E" && fields[whereY][whereX].color==fields[whatY][whatX].color){
+                    cout << RED << "ERROR" << RESET << endl;
+                    return;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else{
+                cout << RED << "Wrong format" << RESET << endl;
+                return;}
 
 
+        } else if (fields[whatY][whatX].name == "B") {
+
+            cout << whatY << endl;
+            cout << whatX << endl;
+            cout << whereY << endl;
+            cout << whereX << endl;
+
+            int tmp_x = whatX;
+            int tmp_y = whatY;
+            if (abs(whereX - whatX) != abs(whereY - whatY)) {
+                cout << RED << "Wrong format" << RESET << endl;
+                return;}
+            else {
+                int dX = (whatX > whereX) ? -1 : 1;
+                int dY = (whatY > whereY) ? -1 : 1;
+                tmp_x += dX;
+                tmp_y += dY;
+                while (tmp_x != whereX && tmp_y != whereY) {
+                    if (fields[tmp_y][tmp_x].name != "E") {
+                        cout << RED << "ERROR" << RESET << endl;
+                        return;}
+                tmp_x += dX;
+                tmp_y += dY;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+
+                }
+    }
+
+        else if (fields[ whatY ][ whatX ].name == "Q") {
+            if(whatX==whereX && whatY!=whereY)
+            {
+                int tmp_x = whatX;
+                int tmp_y = whatY;
+                int dY = (whatY > whereY) ? -1 : 1;
+                tmp_y += dY;
+                while(tmp_y!=whereY)
+                {
+                    if (fields[tmp_y][tmp_x].name != "E") {
+                        cout << RED << "ERROR" << RESET << endl;
+                        return;}
+                    tmp_y += dY;
+                }
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+
+            }
+            else if(whatX!=whereX && whatY==whereY)
+            {
+                int tmp_x = whatX;
+                int tmp_y = whatY;
+                int dX = (whatX > whereX) ? -1 : 1;
+                tmp_x += dX;
+                while(tmp_x!=whereX)
+                {
+                    if (fields[tmp_y][tmp_x].name != "E") {
+                        cout << RED << "ERROR" << RESET << endl;
+                        return;}
+                    tmp_x += dX;
+                }
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+            }
+            else if(abs(whereX - whatX) == abs(whereY - whatY))
+            {
+                int tmp_x = whatX;
+                int tmp_y = whatY;
+                int dX = (whatX > whereX) ? -1 : 1;
+                int dY = (whatY > whereY) ? -1 : 1;
+                tmp_x += dX;
+                tmp_y += dY;
+                while (tmp_x != whereX && tmp_y != whereY) {
+                    if (fields[tmp_y][tmp_x].name != "E") {
+                        cout << RED << "ERROR" << RESET << endl;
+                        return;}
+                    tmp_x += dX;
+                    tmp_y += dY;}
+                fields[whereY][whereX] = fields[whatY][whatX];
+                fields[whatY][whatX].name = "E";
+
+            }
+            else{
+                cout << RED << "Wrong format" << RESET << endl;
+                return;}
 
 
-
-
-
-        } else if (fields[ whatY ][ whatX ].name == "Q") {
 
         } else if (fields[ whatY ][ whatX ].name == "K") {
 
@@ -210,10 +298,6 @@ void Board::turn(string move) {
              * fields[ whatY + dir ][ whatX ]    - the field where we want to move
              * fields[ whatY ][ whatX ]          - the field with a piece we are moving
              */
-
-            string sWhereY = &where[1];
-            int whereY = stoi(sWhereY)-1;
-            int whereX = int(where[0]) - int('a');
 
             int dir = ( fields[ whatY ][ whatX ].color )? -1 : 1;
             cout << "[ move ]: " << fields[ whatY ][ whatX ].name << endl;
@@ -233,7 +317,7 @@ void Board::turn(string move) {
 
 }
 void Board::print() {
-    cout <<BOLDMAGENTA <<  "    | A | B | C | D | E | F | G | H |" << RESET;
+    cout <<BOLDMAGENTA <<  "    | H | G | F | E | D | C | B | A |" << RESET;
     int a = 0;
     for( vector<Field>row : fields ) {
 
@@ -255,5 +339,5 @@ void Board::print() {
 
     }
     cout <<endl<< BOLDMAGENTA << "-----------------------------------------"<< RESET <<endl;
-    cout <<BOLDMAGENTA <<  "    | A | B | C | D | E | F | G | H |" << RESET <<endl;
+    cout <<BOLDMAGENTA <<  "    | H | G | F | E | D | C | B | A |" << RESET <<endl;
 }
